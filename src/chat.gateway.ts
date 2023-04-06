ff8f88b9dd0293f6f135062c75887841dd504173
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-04-06 14:51:47 +0900

    Update chat.gateway.ts

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,12 +1,36 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer , ConnectedSocket  } from "@nestjs/websockets";
+import { Socket } from "socket.io";
 
-@WebSocketGateway()
+
+
+@WebSocketGateway({
+  cors : {
+    origin:'*'
+  }
+})
+
 export class ChatGateway {
   @WebSocketServer()
   server;
 
+  // 실시간 채팅 Communication
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string , nickname:string): void {
-    this.server.emit('message', message);
+  handleMessage(
+    @MessageBody() message: string,
+    ): 
+  void {
+    this.server.to(message.split("방이름")[1]).emit('message', message);
+  }
+
+
+  // 채팅방 생성
+  @SubscribeMessage('room')
+  createroom(
+    @MessageBody() room: string,
+    @ConnectedSocket() client: Socket,
+    ): 
+  void {
+    client.join(room)
+    console.log(room + "이 생성되었습니다.")
   }
 }
